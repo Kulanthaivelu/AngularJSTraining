27837a416066be747cb0c0913582e953efb32220
Kulanthaivelu <kulanthaivelu.v@gmail.com>
2018-02-13 00:57:45 -0800

    updated files

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
 var myapp = angular.module('myapp', []);
-console.log(myapp);
+// myapp.controller('MyCtrl', ['$scope', function($scope){
+//     $scope.myval = "Hello";
+
+//     $scope.onClick = function(){
+//         alert($scope.myval);
+//         $scope.myval = "Some New Value";
+//         alert($scope.myval);
+//     };
+// }])
+
+// myapp.controller('MyCtrl', ['$scope', function($scope){
+//     $scope.students = [
+//         {
+//             name:'Arun',
+//             age:10
+//         },
+//         {
+//             name:'Ajith',
+//             age:11
+//         }
+//     ]
+
+//     $scope.onViewBtnClick =function(stu){
+//         $scope.selectedStudent = stu;
+//     }
+// }])
+
+myapp.controller('MyCtrl', ['$scope', 'AppService', 
+function($scope, AppService){
+    AppService.getStudents()
+        .then(function(result){
+            $scope.students = result.data;
+        });
+    
+    $scope.onViewBtnClick =function(stu){
+        $scope.selectedStudent = stu;
+    }
+}])
